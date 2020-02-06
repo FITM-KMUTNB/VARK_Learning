2785398f70bbe0c2a7ad1939cb42d20bdc8e1b38
Kaow <kaowrg@gmail.com>
2020-02-06 17:04:28 +0700

    encode text

--- varkapp/forms.py
+++ varkapp/forms.py
@@ -5,18 +5,18 @@ from varkapp.models import User
 
 class RegistrationForm(FlaskForm):
     gender = RadioField('Gender',
-                           choices=[('M','Male'),('FM','Female')])
+                            choices=[('M','Male'),('FM','Female')])
     firstname = StringField('First Name',
-                           validators=[DataRequired(), Length(min=5, max=50)])
+                            validators=[DataRequired(), Length(min=5, max=50)])
     lastname = StringField('Last Name',
                             validators=[DataRequired(), Length(min=5, max=50)])
     age = StringField('Age',
                             validators=[DataRequired(), Length(min=1, max=2)])
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                            validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+                            validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, email):
