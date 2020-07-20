67f12d62b5eabc7913fe6944835a07b78f0d958e
Yanakorn Ruamsuk <46589225+YanakornR@users.noreply.github.com>
2020-07-20 13:22:33 +0700

    edit port

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from varkapp import app, manager
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8081)
     #manager.run()
