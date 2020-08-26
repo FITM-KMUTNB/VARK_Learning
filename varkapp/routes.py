3b5b155938243f139389315b30bc780238f23164
Yanakorn Ruamsuk <46589225+YanakornR@users.noreply.github.com>
2020-08-26 13:55:48 +0700

    replace _ to space

--- varkapp/routes.py
+++ varkapp/routes.py
@@ -102,6 +102,7 @@ def display_exercise():
         learntype = request.form['learntype']
 
         #pdf = pdfplumber.open("varkapp/"+testfile)
+        testfile = testfile.replace(' ', '_')
         pdf = pdfplumber.open('~/ROOT/varkapp/'+testfile)
         print(testfile)
         text = []
